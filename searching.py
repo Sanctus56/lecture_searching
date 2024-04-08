19847e551d3cb560bf624f9081ab68b45bc05b34
Sanctus56 <alex.kostolansky32@gmail.com>
2024-04-08 13:23:26 +0200

    Kuk4

--- searching.py
+++ searching.py
@@ -36,10 +36,37 @@ def linear_search(sequence, number):
     return slovník
 
 
+def pattern_search(sequence, searched_pattern):
+    pattern_length = len(searched_pattern)
+    list_of_positions = []
+    number_of_operations = 0
+    for position in range(len(sequence) - pattern_length):
+        is_same = True
+        for subind in range(len(searched_pattern)):
+            number_of_operations = number_of_operations + 1
+            if sequence[position+subind] == searched_pattern[subind]:
+                is_same = is_same and True
+            else:
+                is_same = is_same and False
+                # Break
+        if is_same:
+            list_of_positions.append(position+pattern_length //2)
+    print(number_of_operations)
+    return {'positions': list_of_positions, 'count': len(list_of_positions)}
+
+
+
+
+
+
+
+
+
 def main():
-    sequential_data = read_data("sequential.json", "unordered_numbers")
+    sequential_data = read_data("sequential.json", "dna_sequence") #upravit field podla toho co hladam
     print(sequential_data)
     print(linear_search(sequential_data, 0))
+    print(pattern_search(sequential_data, "ATA"))
     pass
 
 
