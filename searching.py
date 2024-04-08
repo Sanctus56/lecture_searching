8d95af743d6881897049bc0e418c99cd1d0149d0
Sanctus56 <alex.kostolansky32@gmail.com>
2024-04-08 12:41:39 +0200

    Kuk3

--- searching.py
+++ searching.py
@@ -19,10 +19,30 @@ def read_data(file_name, field):
 
 
 
+def linear_search(sequence, number):
+    """
+    Sus
+    """
+    list = []
+    pocet = 0
+    slovník = {'positions': 0, "count": 0}
+    for i in range(len(sequence)):
+        if sequence[i] == number:
+            list.append(i)
+            pocet = pocet + 1
+    slovník['positions'] = list
+    slovník['count'] = pocet
+
+    return slovník
+
+
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print(sequential_data)
+    print(linear_search(sequential_data, 0))
     pass
+
+
 
 
 if __name__ == '__main__':
